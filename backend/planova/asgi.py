d86145dc24488564e7ab8530ddf9a2ef1f3e17da
MelkiAmin <melkiamine@kef.r-iset.tn>
2026-02-24 01:35:09 +0000

    Update backend and frontend files

--- backend/planova/asgi.py
+++ backend/planova/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for nameproject project.
+ASGI config for planova project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nameproject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'planova.settings')
 
 application = get_asgi_application()
